0232d81a3fa4bb1743db25b922d242b7738e30bc
Sharaar <walidsharar@gmail.com>
2022-01-21 18:56:11 +0100

    Create Student table

--- Manipulating.sql
+++ Manipulating.sql
@@ -8,6 +8,16 @@ drop database school
 -- Use Database
 use school
 
+-- Create Table
+
+Create table students(
+sid int primary key not null identity(1,1),
+lastname nvarchar(20) not null,
+firstname nvarchar(20) not null,
+dateofbirth	datetime not null,
+enrollmentdate datetime not null
+)
+
 
 
 
