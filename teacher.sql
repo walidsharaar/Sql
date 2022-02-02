7d3675cff3cee19962da8e0b78f0edc39b908865
Sharaar <walidsharar@gmail.com>
2022-02-02 21:25:57 +0100

    create teacher

---
+++ teacher.sql
@@ -0,0 +1,17 @@
+USE [school]
+GO
+
+
+CREATE TABLE [dbo].[teachers](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Firstname] [nvarchar](50) NULL,
+	[Lastname] [nvarchar](50) NULL,
+	[Datejoined] [datetime] NULL,
+ CONSTRAINT [PK_teacher] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
