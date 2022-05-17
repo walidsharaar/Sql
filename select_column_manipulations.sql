8a6fb0f8a9a7ac2e9aaf51095c29e5c4c22a53cb
Sharaar <walidsharar@gmail.com>
2022-05-17 20:23:16 +0200

    manipulation of columns

--- select_column_manipulations.sql
+++ select_column_manipulations.sql
@@ -9,3 +9,14 @@ from actor;
 select 
 name,Indepyear , 2020 - Indepyear ,population, gnp, gnp/population*1000 as gnp_per_capita 
 from country;
+
+-- Cancatination 
+select 
+concat(name , ' has population ', population) as country_population,
+concat(HeadofState, 'is the head state of ', name) as country_head,
+concat(name,' ',HeadOfState) country_details,
+name,
+HeadOfState,
+IndepYear
+from 
+country;
