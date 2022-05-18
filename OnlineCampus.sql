d003c4c00a692262c7c0a77914f6fd78d88e2c50
Sharaar <walidsharar@gmail.com>
2022-05-18 19:23:11 +0200

    Assignment 4

---
+++ OnlineCampus.sql
@@ -0,0 +1,7 @@
+/** In this exercise we're going to use the OnlineCampus database.
+
+OnlineCampus is a fictional online open course platform that offers paid academic courses.
+
+The OnlineCampus database consists of a single table: Courses - which provides information about the various courses hosted by OnlineCampus
+**/
+
