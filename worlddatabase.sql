7c17cceeb7feb3903a53594a792bd619c3997c58
Sharaar <walidsharar@gmail.com>
2022-05-19 13:20:12 +0200

    filtering in sql

---
+++ worlddatabase.sql
@@ -0,0 +1,14 @@
+---------------------------
+-- SQL WHERE clause
+---------------------------
+
+-- 1. Introduction
+ 
+-- 2. WHERE Clause Example
+
+select * from country 
+where surfaceArea>7000000;
+
+-- 3. WHERE Clause Syntax
+--    WHERE -> column_name -> operator -> value 
+--    * Operator (>, <, !=, =, >=, <=, ..)
