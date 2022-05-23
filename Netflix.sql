4a4df58f6870f7b6ffd5aefc4c528dcf49f84653
Sharaar <walidsharar@gmail.com>
2022-05-23 13:12:54 +0200

    questions added

--- Netflix.sql
+++ Netflix.sql
@@ -11,3 +11,23 @@ movies - lists all of the relevant information regarding Netflix’s movies
 series - lists all of the relevant information regarding Netflix’s series
 
 **/
+
+--[movies] Write a query to display the title, rating, country, release_year, and director, for all movies directed by Michael Bay. Sort the output by release_year (ascending)
+
+--[movies] Write a query to display the title, country, duration_in_minutes, and date_added, for all movies that were added before March 2011. Sort the output by duration_in_minutes (ascending)
+
+--[movies] Write a query to display the title, country, duration_in_minutes, and release_year, for all movies that were released between 2014 and 2016. Sort the output by duration_in_minutes (descending)
+
+--[movies] Write a query to display the title, director, country, and duration_in_minutes, for all movies with duration between 3-4 hours. Sort the output by duration_in_minutes (descending)
+
+--[series] Write a query to display the title, director, rating, num_of_seasons for all series with 10 to 14 seasons. Sort the output by num_of_seasons (descending)
+
+--[series] Write a query to display the title, director, rating, num_of_seasons for all series a. containing value in the director column (diretor not equals null) b. and having num_of_seasons > 7
+
+-- [movies] Write a query to display the title, director, cast, country and rating, for all movies having Ryan Reynolds in their cast
+
+--[movies] Write a query to display the title, director, cast, country and rating, for all movies having Ryan Reynolds and Nicolas Cage in their cast
+
+--[movies] Write a query to display the title, director, cast, country, duration_in_minutes, and rating a. for all movies having PG (Parental Guidance) in their rating b. and movie duration is greater than 3 hours
+
+-- [series] Write a query to display title, director, cast, country, and release_year for all series released in 2014, 2016, or 2018. Sort the output by title (ascending)
