182b017400de4c09f37e82e2c471afa76338c6ac
Sharaar <walidsharar@gmail.com>
2022-05-23 13:08:41 +0200

    File added

---
+++ Netflix.sql
@@ -0,0 +1,13 @@
+/**
+
+In this exercise we’re going to use The Netflix database.
+
+Netflix database consists of tv shows and movies available on Netflix as of 2019.
+
+In this database you’ll find two tables:
+
+movies - lists all of the relevant information regarding Netflix’s movies
+
+series - lists all of the relevant information regarding Netflix’s series
+
+**/
