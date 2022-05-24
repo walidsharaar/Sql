f7607557cc2a016bb423e26d1b256af873ce03cb
Sharaar <walidsharar@gmail.com>
2022-05-24 07:54:35 +0200

    first 3 questions solved

--- Netflix.sql
+++ Netflix.sql
@@ -13,11 +13,21 @@ series - lists all of the relevant information regarding Netflix’s series
 **/
 
 --[movies] Write a query to display the title, rating, country, release_year, and director, for all movies directed by Michael Bay. Sort the output by release_year (ascending)
+select title, rating, country, release_year, director 
+from movies 
+where director = 'Michael Bay'
+order by release_year
 
 --[movies] Write a query to display the title, country, duration_in_minutes, and date_added, for all movies that were added before March 2011. Sort the output by duration_in_minutes (ascending)
-
+select title, country, duration_in_minutes, date_added
+from movies 
+where date_added < '2011-03-01'
+order by duration_in_minutes
 --[movies] Write a query to display the title, country, duration_in_minutes, and release_year, for all movies that were released between 2014 and 2016. Sort the output by duration_in_minutes (descending)
-
+select title, country, duration_in_minutes, release_year
+from movies
+where release_year between 2014 and 2016
+order by duration_in_minutes desc
 --[movies] Write a query to display the title, director, country, and duration_in_minutes, for all movies with duration between 3-4 hours. Sort the output by duration_in_minutes (descending)
 
 --[series] Write a query to display the title, director, rating, num_of_seasons for all series with 10 to 14 seasons. Sort the output by num_of_seasons (descending)
