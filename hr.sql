3c1bbd92e0801e423f8103140e6a9bda97b395f2
Sharaar <walidsharar@gmail.com>
2022-05-25 22:50:20 +0200

    questions added

---
+++ hr.sql
@@ -0,0 +1,36 @@
+/*
+In this exercise we’re going to use the HR database.The HR database consists of a single table - employees, providing details about 1,000 employees working in a global tech-company
+ */
+
+-- 1. Display the id, first name in lowercase and last name in uppercase, for all employees whose employee number is in the range between 80 and 150.
+
+
+
+
+-- 2.For each employee, display the:
+
+--a. first name
+
+--b. last name
+
+--c. email address - The email address will be composed of the first letter of first name, concatenated with the three first letters of last name, concatenated with @gmail.com.
+
+
+
+
+
+
+-- 3. Repeat the last query, only this time present the values of Email in lowercase
+
+
+
+
+-- 4.Write a query to display the id, first_name, last_name and phone_number In each phone_number replace all dashes ('-') with dots ('.')
+
+
+
+
+
+
+
+-- 5. Write a query to display the id, first_name, last_name, job_title, department_id for employees with length of job_title greater than 35 characters
