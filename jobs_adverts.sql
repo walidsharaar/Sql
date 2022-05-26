84908a552f5d7f1164dc137fee717d42a5d0ecbb
Sharaar <walidsharar@gmail.com>
2022-05-26 21:48:33 +0200

    questions resolved

--- jobs_adverts.sql
+++ jobs_adverts.sql
@@ -6,16 +6,25 @@ a. Write a query to display the job_id, job_title, salary_estimate_min, salary_e
 b. Repeat your last query, only this time display only the job adverts where salary_estimate_max is less than 105,000
 */
 
+select job_id,job_title,salary_estimate_min,salary_estimate_max
+from jobs ;
 
 
 
+select job_id, job_title, salary_estimate_min, salary_estimate_max
+from jobs
+where cast(replace(salary_estimate_max,'K','') AS int) < 105;
 
 /*
  2. Write a query to display the company_name, company_rank, company_size_min, and company_size_max
 for companies with more than 60 employees and less than 120 employees
  */
 
-
+select company_name, company_rank, company_size_min,company_size_max
+from jobs 
+where cast(replace(company_size_min,'Employees','') as int)> 60
+and 
+cast(replace(company_size_max,'Employees','') as int)< 120
 
 /*
  3. Write a query to display the :
@@ -28,6 +37,8 @@ c. company_name in lowercase
  
  */
 
+select job_id,upper(job_title),lower(company_name)
+from jobs;
 
 /*
  4. Write a query to display the:
@@ -39,7 +50,10 @@ b. first letter of company_name
 c. first letter of headquarters_of_company
  */
 
-
+select job_id,company_name,headquarters_of_company,
+left(company_name,1) as initial_company,
+left(headquarters_of_company,1) as initial_head
+from jobs;
 
 
 /*
@@ -55,7 +69,9 @@ d. company_code - a new column containing a concatenation of: the first letter o
 For example : for Google located in Austin, the company_code would be: GA
  */
 
-
+select job_id, company_name, headquarters_of_company,
+concat(left(company_name,1),left(headquarters_of_company,1))  as initial
+from jobs;
 
 /*
  6. Repeat your last query, only this time display the company_code in lowercase
@@ -63,14 +79,18 @@ For example : for Google located in Austin, the company_code would be: GA
  */
 
 
-
+select job_id, lower(company_name) as company_name , headquarters_of_company,
+concat(left(company_name,1),left(headquarters_of_company,1))  as initial
+from jobs;
 
 /*
  7. Write a query to display the job titles with length greater than 29
  */
 
 
-
+select distinct job_title  
+from jobs
+where length(job_title)>29;
 
 /*
 8. Write a query to display the:
@@ -87,3 +107,8 @@ e. company_market_value rounded down using FLOOR
 
 f. company_market_value rounded up using ceil
 */
+select company_name,company_rank,company_market_value,
+round(company_market_value,2) as roundedValue,
+floor(company_market_value) as flooredValue,
+ceiling(company_market_value) as ceilValue
+from jobs;
