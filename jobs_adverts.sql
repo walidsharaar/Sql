04a18c7100d1b3edd0f4b7e19563785134850c8c
Sharaar <walidsharar@gmail.com>
2022-05-26 21:25:14 +0200

    Questions added

---
+++ jobs_adverts.sql
@@ -0,0 +1,89 @@
+/*
+1. [String Functions]
+
+a. Write a query to display the job_id, job_title, salary_estimate_min, salary_estimate_max
+
+b. Repeat your last query, only this time display only the job adverts where salary_estimate_max is less than 105,000
+*/
+
+
+
+
+
+/*
+ 2. Write a query to display the company_name, company_rank, company_size_min, and company_size_max
+for companies with more than 60 employees and less than 120 employees
+ */
+
+
+
+/*
+ 3. Write a query to display the :
+
+a. job_id
+
+b. job_title in uppercase
+
+c. company_name in lowercase
+ 
+ */
+
+
+/*
+ 4. Write a query to display the:
+
+a. job_id, company_name, headquarters_of_company
+
+b. first letter of company_name
+
+c. first letter of headquarters_of_company
+ */
+
+
+
+
+/*
+ 5. Write a query to display the :
+
+a. job_id
+
+b. company_name
+
+c. headquarters_of_company
+
+d. company_code - a new column containing a concatenation of: the first letter of company_name and the first letter of headquarters_of_company
+For example : for Google located in Austin, the company_code would be: GA
+ */
+
+
+
+/*
+ 6. Repeat your last query, only this time display the company_code in lowercase
+ 
+ */
+
+
+
+
+/*
+ 7. Write a query to display the job titles with length greater than 29
+ */
+
+
+
+
+/*
+8. Write a query to display the:
+
+a. company_name
+
+b. company_rank
+
+c. company_market_value
+
+d. company_market_value rounded using ROUND function with precision of 2 digits
+
+e. company_market_value rounded down using FLOOR
+
+f. company_market_value rounded up using ceil
+*/
