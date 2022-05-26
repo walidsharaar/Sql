0913f1f54ff4645f8f63023f2a5ef0a7886ab2cc
Sharaar <walidsharar@gmail.com>
2022-05-26 22:51:23 +0200

    questions added

---
+++ airbnb.sql
@@ -0,0 +1,29 @@
+/*
+Since 2008, guests and hosts have used Airbnb to expand on traveling possibilities and present a more unique, personalized way of experiencing the world. 
+The Airbnb database consists of a single table - rooms which describes the listing activity in NYC, NY for 2019.
+*/
+
+-- 1. Display the average room price (across the entire table)
+
+
+
+-- 2. Display the highest number of reviews
+
+
+
+-- 3. Display the lowest reviews_per_month
+
+
+-- 4. Display the number of (not null) values in reviews_per_month
+
+
+
+
+-- 5.List the distinct values in neighbourhood_group (no group function is required, just a simple DISTINCT)
+
+
+
+-- 6. Display the number of unique values in neighbourhood_group
+
+
+
