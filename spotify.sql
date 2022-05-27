7d650137e93f5e4b768d6906d9fb71917603661f
Sharaar <walidsharar@gmail.com>
2022-05-28 00:15:05 +0200

    questions resolved

--- spotify.sql
+++ spotify.sql
@@ -7,17 +7,17 @@ The Spotify database consists of a single table - tracks, which contains audio s
 
 --1. List the number of tracks made by each artist. Sort the output by the number of tracks (Descending
 
-select artist, COUNT(*) as "number_of_tracks"
+select artist, count(*) as "number_of_tracks"
 from tracks 
 group by artist
-order by COUNT(*) desc;
+order by count(*) desc;
 
 --2. Display the average duration of tracks by genre. Sort the output by the average duration (Descending)
 
-select genre, AVG(duration) as "average_duraion"
+select genre, avg(duration) as "average_duraion"
 from tracks 
 group by genre
-order by AVG(duration) desc;
+order by avg(duration) desc;
 
 --3. Display the minimum, maximum, and average danceability of tracks made by Queen and The Beatles
 
@@ -66,4 +66,7 @@ having count(*) > 10;
 
 -- 7. List the number of tracks released by Coldplay each year. Sort the output by release_year (ascending)
 
-
+select artist, release_year, count(*) as "number_of_tracks"
+from tracks
+where artist = 'Coldplay'
+group by artist, release_year;
