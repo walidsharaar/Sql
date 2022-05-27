62bdf59edd53d56e55fb8a90e13cad4858a92a4a
Sharaar <walidsharar@gmail.com>
2022-05-27 23:55:10 +0200

    questions added

---
+++ spotify.sql
@@ -0,0 +1,32 @@
+/*
+Spotify is a digital music streaming service that gives you access to millions of songs, podcasts and videos from artists all over the world.
+
+The Spotify database consists of a single table - tracks, which contains audio statistics of the top 2000 tracks on Spotify. The table contains about 15 columns each describing the track and it's qualities.
+*/
+
+
+--1. List the number of tracks made by each artist. Sort the output by the number of tracks (Descending
+
+
+--2. Display the average duration of tracks by genre. Sort the output by the average duration (Descending)
+
+--3. Display the minimum, maximum, and average danceability of tracks made by Queen and The Beatles
+
+--4. Pop music consists of different genres, for example: Art Pop, Dance Pop, and Candy Pop are all Pop music genres. How many pop music genres are listed in this dataset ?
+
+
+
+--5. Display the number of tracks, highest popularity, and lowest popularity each rock music artist has achieved. Sort the output by the number of tracks (descending)
+
+
+
+
+-- 6.Tracks by genre
+
+--a. List the number of tracks by each genre, for tracks released during 2005-2010.
+
+--b. Further restrict your result to display only genres with more than 10 tracks
+
+
+
+-- 7. List the number of tracks released by Coldplay each year. Sort the output by release_year (ascending)
