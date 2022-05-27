70a0d3891fc43b9ff12eb92911ec005d34b2b7c4
Sharaar <walidsharar@gmail.com>
2022-05-27 20:32:53 +0200

    questions resolved

--- airbnb.sql
+++ airbnb.sql
@@ -5,25 +5,34 @@ The Airbnb database consists of a single table - rooms which describes the listi
 
 -- 1. Display the average room price (across the entire table)
 
-
+select avg(price)
+from rooms;
 
 -- 2. Display the highest number of reviews
 
+select * from rooms;
 
+select max(number_of_reviews)
+from rooms;
 
 -- 3. Display the lowest reviews_per_month
-
+select min(minimum_nights)
+from rooms;
 
 -- 4. Display the number of (not null) values in reviews_per_month
 
-
+select count(reviews_per_month)
+from rooms;
 
 
 -- 5.List the distinct values in neighbourhood_group (no group function is required, just a simple DISTINCT)
 
-
+select distinct neighbourhood_group
+from rooms;
 
 -- 6. Display the number of unique values in neighbourhood_group
 
+select  count(distinct neighbourhood_group)
+from rooms;
 
 
