f27a2c783c6954695313add26b35bdcb277989d8
Sharaar <walidsharar@gmail.com>
2022-05-28 12:11:20 +0200

    questions added

---
+++ BookReviews.sql
@@ -0,0 +1,68 @@
+/*
+In this exercise we’re going to use the BookReview database.
+
+BookReview is a fictional social cataloging website that allows individuals to search its database of books, and rate them (available to registered users only).
+*/
+
+/*
+ * 1. a. Write a query to display the first_name, last_name, email, birthdate, and gender for all users
+
+b. Restrict your query to display only users who were born on 2002
+
+c. Write a query to display the number of users who were born each year. Sort the output by year (ascending)
+d. Repeat your last query, only this time display only years with more than 54 users (who were born on that year)
+ */
+
+
+
+
+
+
+/*
+ * 2.a. Write a query to display the first_name, last_name, email, birthdate, country and city for all users
+
+b. Restrict your query to display only users living in the UK
+
+c. Write a query to break down the number of users living in each country and city. Sort the output by the number of users (descending)
+
+d. Restrict your query to count only users above the age 30
+ */
+
+
+
+
+
+/*
+  3. a. Write a query to display the first_name, last_name, email, age, and occupation_name for all users
+
+b. Write a query to display the average age for each occupation
+
+c. Restrict your query to display only occupations with average age greater than 40
+ */
+
+
+
+
+
+
+/*
+ 4.Write a query to display the average num_pages for each author. Sort the output by the average number of pages (descending)
+ */
+
+
+
+
+
+/*
+5.Write a query to display the number of books for each language, Sort the output by the number of books (descending)
+ */
+
+
+
+
+
+
+
+/*
+6. Write a query to display the number of unique publishers by each language, Sort the output by the number of unique publishers (descending)
+ */
