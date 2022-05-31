191d9acc993f1b51ff6d9a0c7e6cdca76d9143da
Sharaar <walidsharar@gmail.com>
2022-05-31 15:45:55 +0200

    all questions resolved

--- carsOnline.sql
+++ carsOnline.sql
@@ -112,8 +112,30 @@ c. Display the number of customers by each gender and age. Sort the output by th
 d. Restrict your query to for customers above the age 59
  
  */
+select c.customer_id, c.first_name, c.last_name, c.birth_date, g.gender
+from customers c join genders g
+on   c.gender_code = g.gender_code;
 
+select g.gender, count(*) as "number_of_customers"
+from customers c join genders g
+on   c.gender_code = g.gender_code
+group by g.gender;
 
+select g.gender, 
+       date_part('year', current_date) - date_part('year', c.birth_date) as "age", 
+       count(*) as "number_of_customers"
+from customers c join genders g
+on   c.gender_code = g.gender_code
+group by g.gender, date_part('year', current_date) - date_part('year', c.birth_date)
+order by count(*) desc;
+
+select g.gender, date_part('year', current_date) - date_part('year', c.birth_date) as "age", 
+       count(*) as "number_of_customers"
+from customers c join genders g
+on   c.gender_code = g.gender_code
+where date_part('year', current_date) - date_part('year', c.birth_date) > 59
+group by g.gender, date_part('year', current_date) - date_part('year', c.birth_date)
+order by count(*) desc;
 /*
  7.
  a. Write a query to display the number of customers living in Australia .
@@ -123,6 +145,22 @@ b. Write another query to display the number of customers with updated phone_num
 c. Write another query to display the number of australian customers with NULL value in their phone_number, break down the result for each city, sort it by the count (descending).
  */
 
+select count(*) as "number_of_customers"
+from customers c join locations l 
+on   c.location_code = l.location_code
+where l.country = 'Australia';
+
+select count(phone_number) as "number_of_customers"
+from customers c join locations l 
+on   c.location_code = l.location_code
+where l.country = 'Australia';
+
+select city, count(*) as "number_of_customers"
+from customers c join locations l 
+on   c.location_code = l.location_code
+where l.country = 'Australia' and phone_number is null
+group by city
+order by count(*) desc;
 
 
 
@@ -131,17 +169,34 @@ c. Write another query to display the number of australian customers with NULL v
  Write a query to display the customer_id and full name of customers who bought more than 5 cars
  */
 
+select c.customer_id, concat(c.first_name,'  ' ,c.last_name) as "full_name", count(*) as "number_of_cars"
+from sales s join customers c 
+on s.customer_id = c.customer_id
+group by c.customer_id, concat(c.first_name,'  ', c.last_name)
+having count(*) > 5 ;
 
 /*
  9.
  Not every car on the cars table has been sold. Write a query to display the percent of sold cars.
  */
 
+select count(s.customer_id) / cast(count(*) as decimal) * 100 as "percent_of_sold_cars"
+from sales s right outer join cars c 
+on s.car_id = c.car_id;
 
 
 /*
  10.
  On 2019 (of purchase date), What was the average price of sold cars made by Audi?
  */
+
+select avg(cr.price) as "average_price"
+from sales s join cars cr 
+on s.car_id = cr.car_id
+			  join car_types ct
+on  ct.car_type_id = cr.car_type_id
+where date_part('year', s.purchase_date) = 2019 and ct.car_make = 'Audi';
+
+
 
 
