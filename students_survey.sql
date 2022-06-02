c77417d3c31a85612d151741bdbd3f7bc8686048
Sharaar <walidsharar@gmail.com>
2022-06-02 21:30:12 +0200

    questions added

---
+++ students_survey.sql
@@ -0,0 +1,27 @@
+/*
+ The students_survey database consists of a single table - survey, which contains fictional data which were obtained in a survey of students
+ */
+
+
+-- 1. Write a query to display the student_id, full_name and university_id for all students who go to the same university as student_id 6043
+
+-- 2. Write a query to display the survey_id and survey_date for all surveys that were taken on the same date as survey_id 532
+
+-- 3. Write a query to display the student_id, full_name and height for all students who are taller than Josephina Athow (full_name)
+
+-- 4. Write a query to display the student_id, full_name and age for all students younger than the student who filled survey number 10
+
+-- 5. Write a query to display the student_id, full_name, and father_job, for all students whose father works in the same job as (the father of) Christina Rediers (full_name)
+
+/* 6. Write a query to display the student_id, full_name, father_job, and mother_job for all students whose:
+
+a. Father works in the same job as (the father of) Christina Rediers and
+
+b. Mother works in the same job as (the mother of) Christina Rediers and
+*/
+
+-- 7. Write a query to display the student_id, full_name and age for all students older than the average
+
+
+
+-- 8. Write a query to display the student_id, full_name, age and height for all students older than the average, and taller than the average
